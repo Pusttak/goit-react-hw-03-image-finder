d4069d9f61908623423b34df1e9313f97b020163
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-05-18 20:03:58 +0400

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import LoadMoreButton from 'components/LoadMoreButton';
+import Loader from 'components/Loader';
+import Modal from 'components/Modal';
 import s from './App.module.css';
+
+const API_KEY = '26702272-2d7e64543fb5f8670261a42e5';
+const BASE_URL = 'https://pixabay.com/api/';
 
 class App extends Component {
   state = {
     imageName: '',
+    images: [],
+    page: 1,
+    loader: false,
+    activeImg: '',
+    showModal: false,
+    totalImages: 0,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const url = `${BASE_URL}?key=${API_KEY}&q=${this.state.imageName}&page=${this.state.page}&per_page=12&image_type=photo&orientation=horizontal`;
+
+    if (
+      prevState.imageName !== this.state.imageName &&
+      this.state.imageName !== ''
+    ) {
+      this.setState({ images: [], page: 1 });
+      this.fetchImages(url);
+    }
+
+    if (prevState.page !== this.state.page && this.state.page !== 1) {
+      this.fetchImages(url);
+    }
+  }
+
+  fetchImages = url => {
+    this.setState({ loader: true });
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(new Error(response.status));
+      })
+      .then(images => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images.hits],
+          totalImages: images.totalHits,
+        }));
+        if (this.state.page === 1) {
+          if (images.totalHits === 0) {
+            toast.error('Nothing found');
+          } else {
+            toast.success(`Found ${images.totalHits} images`);
+          }
+        }
+      })
+      .catch(err => console.log(err))
+      .finally(() => {
+        this.setState({ loader: false });
+      });
   };
 
   onSubmit = imageName => {
@@ -14,13 +71,48 @@ class App extends Component {
     });
   };
 
+  incrementPage = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  setActiveImg = imageUrl => {
+    this.setState({
+      activeImg: imageUrl,
+    });
+  };
+
+  toggleModal = () =>
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+
   render() {
-    const { imageName } = this.state;
+    const { images, loader, showModal, activeImg, page, totalImages } =
+      this.state;
+    const loadMore = totalImages / 12 > page;
 
     return (
       <div className={s.App}>
+        <Toaster position="top-right" reverseOrder={false} />
         <Searchbar onSubmit={this.onSubmit} />
-        <ImageGallery imageName={imageName} />
+        {images.length !== 0 && (
+          <ImageGallery
+            images={images}
+            setImageModal={this.setActiveImg}
+            onOpenModal={this.toggleModal}
+          />
+        )}
+        {loader && <Loader />}
+        {images.length !== 0 && loadMore && (
+          <LoadMoreButton onClick={this.incrementPage} />
+        )}
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={activeImg} alt="" />
+          </Modal>
+        )}
       </div>
     );
   }
